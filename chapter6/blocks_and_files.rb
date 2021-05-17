36ee239f69d6f8ea07dbd9fc377c40c825f962de
Mal <malsdepot@gmail.com>
2021-05-16 21:51:07 -0400

    Chapter 6 complete

--- chapter6/blocks_and_files.rb
+++ chapter6/blocks_and_files.rb
@@ -5,10 +5,26 @@
 #lines = review_file.readlines
 #review_file.close
 lines = []
+adjectives = []
 
 File.open("reviews.txt") do |review_file|
   lines=review_file.readlines
 end
 
 relevant_lines = lines.find_all { |line| line.include?("Truncated") }
+reviews = relevant_lines.reject { |line| line.include?("--") }
+
+def find_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
+
+adjectives = reviews.map do  |review| 
+  adjective = find_adjective(review)
+  "'#{adjective.capitalize}'"
+end
+
+puts "The critics agree, Truncated is:"
+puts adjectives
 
